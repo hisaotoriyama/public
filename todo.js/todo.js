c0dca09f462aabd4f461963cbd389e27fa1eb1b1
hisaotoriyama <hisao.toriyama@gmail.com>
2021-02-10 23:25:25 +0900

    first draft of todo list

--- todo.js/todo.js
+++ todo.js/todo.js
@@ -1,6 +1,36 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
-    }
+    　todos: [],
+    　newToDo:""
+      },
+    methods: {
+        addNewTodo: function(){
+            const newToDo = {
+                todo: this.newToDo,
+                done: false
+            }
+            alltodos = this.loadTodo()
+            alltodos != null ? alltodos.push(newToDo) : alltodos = [newToDo]
+            localStorage.setItem('todoList', JSON.stringify(alltodos));
+            this.todos = alltodos
+            this.newToDo = ""
+        },
+        deleteTodos: function(){
+            vvv = this.todos.filter(function(v) {return v["done"] === false})
+            localStorage.setItem('todoList', JSON.stringify(vvv));
+            this.todos = this.loadTodo()
+        },
+        loadTodo: function(){
+            var JSONalltodos = localStorage.getItem('todoList');
+            return JSON.parse(JSONalltodos)
+        }
+    },
+    created:
+    function(){
+            this.todos = this.loadTodo()
+        },
+
   })
+
+
